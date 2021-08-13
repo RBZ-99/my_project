3829e596e4eedbfe718627b6eecfe789bcae2ab8
RBZ-99 <rushikeshzawar10@gmail.com>
2021-08-14 03:10:04 +0530

    my changes new

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Rushikesh Zawar',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Rushikesh Zawar is a student at BITS Pilani, Pilani Campus, India',
+    siteUrl: 'https://rbz-99.github.io/my_project', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'RushikeshZawar',
+        short_name: 'RushikeshZawar',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
