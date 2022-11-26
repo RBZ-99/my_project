e8ef4c2a2f8c28737c778416160bef15dd58a257
Rushikesh Zawar <rushikesh@Rushikeshs-MacBook-Pro.local>
2022-11-27 04:58:13 +0530

    28nov

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -397,7 +397,7 @@ const Featured = () => {
 
                 <div className="project-image">
                   <a href={external ? external : github ? github : '#'}>
-                    <GatsbyImage image={image} alt={title} className="img" />
+                    <GatsbyImage image={image} alt={title}  />
                   </a>
                 </div>
               </StyledProject>
