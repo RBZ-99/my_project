3513f357ad29ce7b49bacccd6d9b17201b5196b6
Rushikesh Zawar <rushikesh@Rushikeshs-MacBook-Pro.local>
2022-11-28 00:17:17 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0bb83b359f1af93a8358.js"
+    "url": "webpack-runtime-fae477f8b3772977cc5e.js"
   },
   {
     "url": "framework-5aa5bd659bcb5481e6d5.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4e7126d4bce8307adb7afb5648685f73"
+    "revision": "c24a32aded23159cd0c307434a06c267"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-67fad2d33d3427007843.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f54e583510377756f660e1e3813f604b"
+    "revision": "02fcfba69f8ec22be43ba6616f78b3d6"
   },
   {
     "url": "polyfill-de21e80b1e89328f6e6c.js"
